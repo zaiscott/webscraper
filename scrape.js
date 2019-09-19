09019c86020235eb7a06ae9ef6d5fb940b2ae6f7
Henry Scott <henry.scott@centre.edu>
2019-09-19 15:44:44 -0400

    scrape

--- scrape.js
+++ scrape.js
@@ -2,4 +2,18 @@ var scraper = require('./tools').scraper;
 //scraper = function(url,parseFcn,filename)
 //scrapes a URL, parses it with parseFcn, and archives the data in a JSON file called filename.
 
+var marvel = function($,data)
+{
+    var parent = $("#comics-characterindex > section.module.moduColor_Light.modu_AZ > div:nth-child(4) > div > div:nth-child(2)");
+    var children = parent.find("li");
+    children.toArray()
+    .forEach(function(item,index)
+    {
+        data.push($(item).text());
+    });
+    
+}
 
+scraper("https://www.marvel.com/comics/characters", marvel, "heroes.json")
+
+// #comics-characterindex > section.module.moduColor_Light.modu_AZ > div:nth-child(4) > div > div:nth-child(2)
